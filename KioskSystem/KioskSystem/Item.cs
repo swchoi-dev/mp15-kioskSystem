58a8e001991ba89dc5f8bdfcfd03edfec594dd86
swchoi-dev <choi.sungwon.179.70@gmail.com>
2026-08-28 14:11:31 +0900

    [Feat] 상품 클래스 할인정보, 갯수 필드 추가

--- KioskSystem/KioskSystem/Item.cs
+++ KioskSystem/KioskSystem/Item.cs
@@ -6,17 +6,23 @@ public abstract class Item
     protected string _name;
     protected ItemCategory _itemCategory;
     protected int _price;
+    protected Promotion _promotion;
+    protected int _count;
 
     public int Id => _id;
     public string Name => _name;
     public ItemCategory ItemCategory => _itemCategory;
     public int Price => _price;
+    public Promotion Promotion => _promotion;
+    public int Count => _count;
     
-    public Item(int id, string name, int price, ItemCategory itemCategory)
+    public Item(int id, string name, int price, ItemCategory itemCategory, Promotion promotion, int count = 0)
     {
         _id = id;
         _name = name;
         _price = price;
         _itemCategory = itemCategory;
+        _promotion = promotion;
+        _count = count;
     }
 }
