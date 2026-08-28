1043cd572530aa872040f25c6c3ea98ad7cb340c
swchoi-dev <choi.sungwon.179.70@gmail.com>
2026-08-28 11:13:43 +0900

    [Feat] 사용자 입력에 따른 함수 호출 추가

--- KioskSystem/KioskSystem/Program.cs
+++ KioskSystem/KioskSystem/Program.cs
@@ -1,7 +1,59 @@
 ﻿// https://github.com/swchoi-dev/mp15-kioskSystem
-class Program  
-{  
-    static void Main(string[] args)  
-    {  
-    }  
+
+using KioskSystem;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isStoreOpen = true;
+        while (isStoreOpen)
+        {
+            Console.Clear();
+
+            int userInput = ConsoleInput.ReadIntInRange("번호 : ", 1, 4);
+            switch (userInput)
+            {
+                case 1:
+                    // 메뉴번호와 수량을 묻기
+                    OrderItem();
+                    break;
+                case 2:
+                    // 장바구니 통째로 비우기
+                    ClearShoppingCart();
+                    break;
+                case 3:
+                    // 합계 금액 출력, 받은 금액 묻기
+                    PayShoppingCart();
+                    break;
+                case 4:
+                    // 그날의 총 주문건수와 총 매출액 출력
+                    isStoreOpen = false;
+                    CloseStore();
+                    break;
+            }
+
+            ConsoleInput.Pause();
+        }
+    }
+
+    static void OrderItem()
+    {
+        Console.WriteLine("상품 담기");
+    }
+
+    static void ClearShoppingCart()
+    {
+        Console.WriteLine("장바구니 비우기");
+    }
+
+    static void PayShoppingCart()
+    {
+        Console.WriteLine("결제");
+    }
+
+    static void CloseStore()
+    {
+        Console.WriteLine("영업 종료");
+    }
 }
