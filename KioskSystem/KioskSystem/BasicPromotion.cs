def828811bdc144a3837fc89ce46bd994c4ab6e8
swchoi-dev <choi.sungwon.179.70@gmail.com>
2026-08-28 15:07:32 +0900

    [Feat] 기본 할인 BasicPromotion 클래스 추가

--- KioskSystem/KioskSystem/BasicPromotion.cs
+++ KioskSystem/KioskSystem/BasicPromotion.cs
@@ -2,13 +2,22 @@ namespace KioskSystem;
 
 public class BasicPromotion : Promotion
 {
+    private const int DISCOUNT_RATE = 10;
+    private const int DISCOUNT_CONDITION = 3;
+    
     public BasicPromotion(string name, string description) : base(name, description)
     {
         
     }
 
-    public override int PromotionPrice(int price, int count)
+    public override int Price(int price, int count)
     {
-        return price;
+        int result = price;
+        if (count >= DISCOUNT_CONDITION)
+        {
+            int discount = price / DISCOUNT_RATE;
+            result -= discount;
+        }
+        return result;
     }
 }
