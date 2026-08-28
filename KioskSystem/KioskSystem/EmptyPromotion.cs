7e45be7c124d208ecc93bbe10469ce5b3e9c39bb
swchoi-dev <choi.sungwon.179.70@gmail.com>
2026-08-28 16:16:14 +0900

    [Feat] EmptyPromotion 클래스 추가

---
+++ KioskSystem/KioskSystem/EmptyPromotion.cs
@@ -0,0 +1,15 @@
+namespace KioskSystem;
+
+public class EmptyPromotion : Promotion
+{
+    public EmptyPromotion()
+    {
+        _name = "프로모션 없음";
+        _description = "정가";
+    }
+    public override int Price(int price, int count)
+    {
+        int result = price * count;
+        return result;
+    }
+}
