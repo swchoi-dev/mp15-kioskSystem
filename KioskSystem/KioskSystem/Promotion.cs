b3050eb9c6790698be84966af91b37beb7e2853f
swchoi-dev <choi.sungwon.179.70@gmail.com>
2026-08-28 14:10:46 +0900

    [Feat] 할인정보 프로모션 추상 클래스 추가

---
+++ KioskSystem/KioskSystem/Promotion.cs
@@ -0,0 +1,18 @@
+namespace KioskSystem;
+
+public abstract class Promotion
+{
+    private string _name;
+    private string _description;
+
+    public string Name => _name;
+    public string Description => _description;
+    
+    public Promotion(string name, string description)
+    {
+        _name = name;
+        _description = description;
+    }
+
+    public abstract int PromotionPrice(int price, int count);
+}
